e5f5cd3a4c12fc67389e3bb9d341ec174d85035a
Dhruv Verma <dhruv2scs@gmail.com>
2019-08-25 13:15:26 +0530

    Updates

---
+++ precache-manifest.038f3cb3ee64420802bf5dabf0e4df38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8306393d7130ae3bf036fe167fb903b7",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "5a187f230384681afa00",
+    "url": "/mywebsite/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "a0b2d6ed498e7c7d198a",
+    "url": "/mywebsite/static/css/main.ff8943ac.chunk.css"
+  },
+  {
+    "revision": "5a187f230384681afa00",
+    "url": "/mywebsite/static/js/2.bebf74cc.chunk.js"
+  },
+  {
+    "revision": "a0b2d6ed498e7c7d198a",
+    "url": "/mywebsite/static/js/main.92fc926f.chunk.js"
+  },
+  {
+    "revision": "a8f2e89e572ebb9ec10c",
+    "url": "/mywebsite/static/js/runtime~main.6cf47a3d.js"
+  },
+  {
+    "revision": "894e2d1047cd691b10d273c4be86dc53",
+    "url": "/mywebsite/static/media/myimage.894e2d10.jpg"
+  }
+]);
