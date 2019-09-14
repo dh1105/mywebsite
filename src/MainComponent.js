141c71a136ac740e7b98653ac49f327e1e10635f
Dhruv Verma <dhruv2scs@gmail.com>
2019-09-14 18:18:42 +0530

    Using browser router for gh-pages fix

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Switch, Route, Redirect, HashRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect, HashRouter } from 'react-router-dom';
 import { history } from './_helpers';
 import Home from './Home/HomeComponent'
 // import AboutMe from './AboutMe/AboutMeComponent'
@@ -12,18 +12,16 @@ class Main extends Component {
     render() {
         // const { alert } = this.props;
         return (
-            <HashRouter basename="/">
-                <Router history={history}>
-                    <Switch>
-                        <Route path="/home" component={Home} />
-                        {/* <Route path="/about" component={AboutMe} /> */}
-                        <Route path="/resume" component={Resume} />
-                        <Route path="/projects" component={Projects} />
-                        <Route path="/contact" component={Contact} />
-                        <Redirect to="/home" />
-                    </Switch>
-                </Router>
-            </HashRouter>
+            <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
+                <Switch>
+                    <Route path="/home" component={Home} />
+                    {/* <Route path="/about" component={AboutMe} /> */}
+                    <Route path="/resume" component={Resume} />
+                    <Route path="/projects" component={Projects} />
+                    <Route path="/contact" component={Contact} />
+                    <Redirect to="/home" />
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
