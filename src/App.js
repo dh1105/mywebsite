c8c0c1ed2f819eb4ffc62950b916df2ead52cc19
Dhruv Verma <dhruv2scs@gmail.com>
2019-10-05 10:40:01 +0530

    Added google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import ReactGA from 'react-ga';
 import Main from './MainComponent'
+import { history } from './_helpers';
+
+// function initializeReactGA() {
+  
+//   ReactGA.pageview('/home');
+// }
 
 function App() {
+  ReactGA.initialize('UA-149467735-1');
+  history.listen(location => ReactGA.pageview(location.pathname));
   return (
     // <div className="App">
     //   <header className="App-header">
